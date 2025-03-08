856a2ec86bea9aa4eb7a3c91f8eba8329aaf5a33
Thundervoltgh1 <eniolusi@gmail.com>
2025-03-08 14:28:40 +0000

    Update multiplicationtable.py

--- multiplicationtable.py
+++ multiplicationtable.py
@@ -4,29 +4,23 @@ new=Tk()
 new.title("MULTIPILCATION TABLE GENERATOR")
 title=Label(new,text="Multiplication table")
 title.grid(row=0,column=3,columnspan=3,pady=25)
-h=Label(new,text="number and range")from tkinter import*
-from tkinter.ttk import*
-new=Tk()
-new.title("MULTIPILCATION TABLE GENERATOR")
-title=Label(new,text="Multiplication table")
-title.grid(row=0,column=3,columnspan=3,pady=25)
 h=Label(new,text="number and range")
 h.grid(row=2,column=0,padx=10)
-z=IntVar
+z=IntVar()
 u=Combobox(new,textvariable=z,width=5)
 u["values"]=tuple(range(101))
 u.grid(row=2,column=1)
-f=IntVar
-g=Radiobutton(new,text="10",variable=f,value=10)
-g1=Radiobutton(new,text="20",variable=f,value=20)
-g2=Radiobutton(new,text="30",variable=f,value=30)
+endVal=IntVar()
+g=Radiobutton(new,text="10",variable=endVal,value=10)
+g1=Radiobutton(new,text="20",variable=endVal,value=20)
+g2=Radiobutton(new,text="30",variable=endVal,value=30)
 g.grid(row=2,column=3)
 g1.grid(row=3,column=3)
 g2.grid(row=4,column=3)
 def generate():
     Tables=""
-    for i in range(f.get()+1):
-        Tables+=str(z.get())+"x"+str(f)+"="+str(z*f)+"\n"
+    for i in range(endVal.get()+1):
+        Tables+=str(z.get())+"x"+str(i)+"="+str(z.get()*i)+"\n"
     c.configure(text=Tables)
 
 button=Button(new,text="Generate",command=generate)
@@ -34,21 +28,4 @@ button.grid(row=5,column=2)
 c=Label(new,anchor="center")
 c.grid(row=6,column=1,pady=25)
 
-new.mainloop()
-h.grid(row=2,column=0,padx=10)
-l=IntVar
-u=Combobox(new,textvariable=l,width=5)
-u["values"]=tuple(range(101))
-u.grid(row=2,column=1)
-n=IntVar
-g=Radiobutton(new,text="10",variable=n,value=10)
-g1=Radiobutton(new,text="20",variable=n,value=20)
-g2=Radiobutton(new,text="30",variable=n,value=30)
-g.grid(row=2,column=3)
-g1.grid(row=3,column=3)
-g2.grid(row=4,column=3)
-button=Button(new,text="Generate",command="")
-button.grid(row=5,column=2)
-c=Label(new,anchor="center")
-c.grid(row=6,column=1,pady=25)
 new.mainloop()
